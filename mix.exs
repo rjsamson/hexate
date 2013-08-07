e5411337419748ec28a730aaf548da0d1bf0574a
Robert J Samson <rjsamson@me.com>
2013-08-07 13:24:12 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Hex.Mixfile do
 
   def project do
     [ app: :hex,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 0.10.1",
       deps: deps ]
   end
--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name: "hex",
                  description: "A simple module for encoding and decoding Hex strings in Elixir.",
-                 version: "0.1.1", keywords: [], 
+                 version: "0.2.0", keywords: [], 
                  maintainers: [[name: "Rob Samson", 
                                 email: "rjsamson@me.com"]],
                  repositories: [[github: "rjsamson/hex.ex"]])
