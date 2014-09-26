3c7b095e1bb5c1548467fbf4076f70519d8a4e25
David Parry <david.parry@suranyami.com>
2014-09-26 17:40:53 +1000

    Oops. Forgot to update the hex.pm package too.
    That's probably why the deps didn't download.

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name: "hex",
                  description: "A simple module for encoding and decoding Hex strings in Elixir.",
-                 version: "0.4.0", keywords: [], 
-                 maintainers: [[name: "Rob Samson", 
+                 version: "0.5.0", keywords: [], 
+                 maintainers: [[name: "Rob Samson",
                                 email: "rjsamson@me.com"]],
                  repositories: [[github: "rjsamson/hex.ex"]])
