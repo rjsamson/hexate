f1d7d467827652b47c572a8c89799c21f73b80f2
David Parry <david.parry@suranyami.com>
2014-09-26 17:28:31 +1000

    Bumped the version up to 0.4.1
    I couldn't figure out why my changes weren't being pulled down into another
    app. This was why. Duh.

--- mix.exs
+++ mix.exs
@@ -3,18 +3,15 @@ defmodule Hex.Mixfile do
 
   def project do
     [ app: :hex,
-      version: "0.4.0",
-      elixir: ">= 0.12.0",
+      version: "0.4.1",
+      elixir: ">= 0.15.1",
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     []
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     []
   end
